dba4b3f473a6c579a918965e7113adfafc5a554c
João Tosetti <94083700+JoaoTosetti@users.noreply.github.com>
2022-05-12 20:13:09 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Anime</title>
+</head>
+<body>
+    
+    <h1>Faça seu login:</h1>
+
+        <input type="email" name="email" size="40" value="" placeholder="Exemplo.123@gmail.com"> <br>
+
+        <input type="password" name="senha" size="40" value="" placeholder="Digite sua senha"> <br>
+
+
+    <a href="#" class = "botaoEntrar">Entrar</a>
+
+
+</body>
+</html>
